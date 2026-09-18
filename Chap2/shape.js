b8937512a377433d9d7f2de9e6b995c3b6cb1880
Luka <lukamattila@gmail.com>
2026-09-18 15:07:17 +0300

    Tehtävä3

---
+++ Chap2/shape.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Person {
+    name = "";
+}
+const jill = {
+    name: "Jill"
+};
+const person = jill;
+console.log(person);
+//# sourceMappingURL=shape.js.map
