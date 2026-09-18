92f61737cfdae950e157afe38e1d70e8e8cdc06c
Luka <lukamattila@gmail.com>
2026-09-18 14:55:35 +0300

    Tehtävä2

---
+++ Chap1/test-age.js
@@ -0,0 +1,14 @@
+"use strict";
+function canDrive(usr) {
+    console.log("user is", usr.name);
+    if (usr.age >= 18) {
+        console.log("allow to drive");
+    }
+    else {
+        console.log("do not allow to drive");
+    }
+}
+const tom = {
+    name: "tom"
+};
+canDrive(tom);
